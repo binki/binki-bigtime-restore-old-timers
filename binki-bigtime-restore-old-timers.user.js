4376a157de05a04fa52dea72b88cef9725c7f763
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2026-08-10 11:42:35 -0400

    Add support for the new frames GUI.
    The new GUI released today pretty much never loads (it literally spends minutes waiting for multiple APIs, many for irrelevant services, which take 15s+ to return data and sometimes a “required” API fails and the load must be restarted). So avoiding full page navigation is critical. Some work was required to trigger iframe navigation. It works but it probably is against the design of the application.
    
    Fixes #1.

--- binki-bigtime-restore-old-timers.user.js
+++ binki-bigtime-restore-old-timers.user.js
@@ -6,6 +6,7 @@
 // @match https://*.bigtime.net/bigtime/*
 // @match https://*.bigtime.net/Bigtime
 // @match https://*.bigtime.net/Bigtime/*
+// @match https://*.bigtime.net/frame*
 // @require https://github.com/binki/binki-userscript-when-element-query-selector-async/raw/0a9c204bdc304a9e82f1c31d090fdfdf7b554930/binki-userscript-when-element-query-selector-async.js
 // ==/UserScript==
 
@@ -15,11 +16,26 @@
   // so test for that.
   if (/^[^:]+:\/\/www\./.test(document.URL)) return;
 
-  const timersButton = await whenElementQuerySelectorAsync(document.body, 'button[data-testid=navbar-timersButton]');
+  // Old UI uses navbar-timersButton, new UI uses timers-icon-button.
+  const timersButton = await whenElementQuerySelectorAsync(document.body, 'button[data-testid=navbar-timersButton], button[data-testid=timers-icon-button]');
   
   // Implement the link to the correct timers.
   timersButton.addEventListener('click', e => {
-    document.location = document.location.toString().replace(/#.*/, '#/timers');
+    const replaceUri = f => {
+      if (/^(?i:[^/]+\/\/[^/]+\/frame)/.test(document.URL)) {
+        // New GUI with frames.
+        const uri = new URL(document.URL);
+        const fakeBase = 'https://iq.bigtime.net/';
+        const frameUri = f(fakeBase + uri.searchParams.get('iq'));
+        const iq = frameUri.substring(fakeBase.length);
+        uri.searchParams.set('iq', iq);
+        history.replaceState(history.state, '', uri.toString());
+        document.querySelector('#layout_container iframe').src = frameUri;
+      } else {
+        document.location = f(document.location.toString());
+      }  
+    };
+    replaceUri(uri => uri.replace(/#.*/, '#/timers'));
     e.preventDefault();
     e.stopPropagation();
   });
