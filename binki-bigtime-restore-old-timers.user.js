3a8b3699b0f9096402bc94b3d9c4ceeb65cd5615
Nathan Phillip Brink <binki@NathannoMacBook-Air.local>
2026-08-18 10:34:37 -0400

    binki-bigtime-restore-old-timers-1.0.3

--- binki-bigtime-restore-old-timers.user.js
+++ binki-bigtime-restore-old-timers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name binki-bigtime-restore-old-timers
 // @homepageURL https://github.com/binki/binki-bigtime-restore-old-timers
-// @version 1.0.2
+// @version 1.0.3
 // @match https://*.bigtime.net/bigtime
 // @match https://*.bigtime.net/bigtime/*
 // @match https://*.bigtime.net/Bigtime
